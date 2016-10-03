87e83630d947230b97615646aefbe5dcda76a038
Mikael <miketekle14@gmail.com>
2016-10-03 12:47:19 -0700

    created working geofence

--- GymFinder/ViewController.swift
+++ GymFinder/ViewController.swift
@@ -27,12 +27,23 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.addAnnotation))
         mapView.addGestureRecognizer(tapGesture)
         
-        let region = CLCircularRegion(center: (location?.coordinate)!, radius: 1000, identifier: "Folsem Office")
-        self.mapView.addOverlays(MKCircle(center: (location?.coordinate)!, radius: 100))
+        
+        let coordinates = CLLocationCoordinate2D(latitude: 37.78494283, longitude: -122.39712273)
+        let region = CLCircularRegion(center: coordinates, radius: 1000, identifier: "Folsem Office")
+        self.mapView.add(MKCircle(center: coordinates, radius: 500))
         
         region.notifyOnEntry = true
         region.notifyOnExit = true
         
+    }
+    
+    //TODO: Display circle over lay
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if let circle = overlay as? MKCircle {
+            let renderer = MKOverlayRenderer.init(overlay: circle)
+            return renderer
+        }
+        return MKOverlayRenderer()
     }
 
     func setup() {
@@ -42,16 +53,32 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationManager.startUpdatingLocation()
         mapView.showsUserLocation = true
     }
+
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        let alert = UIAlertController(title: "Welcome!", message: "You made it to the office :), please have breakfast on us!", preferredStyle: UIAlertControllerStyle.alert)
+        let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(alertAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        let alert = UIAlertController(title: "Goodbye!", message: "We will see you later!", preferredStyle: UIAlertControllerStyle.alert)
+        let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(alertAction)
+        self.present(alert, animated: true, completion: nil)
+    }
     
     //delegate method that fires when user location is recieved
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
         
         self.location = userLocation.location
-        
+
         //zoom in on user
         let region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 1000, 1000)
         mapView.setRegion(region, animated: true)
-        
+//        print(location)
+    
     }
     
     //method that reverse geocodes address asynchronously and creates annotation with that address synchronously
