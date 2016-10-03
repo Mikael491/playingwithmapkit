57432262bb44db11cc6c1d9da67a538c23d3e4e4
Mikael <miketekle14@gmail.com>
2016-10-02 19:46:55 -0700

    created subclass class for annotation, conformed to protocol

--- GymFinder/Annotation.swift
+++ GymFinder/Annotation.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import MapKit
+
+class Annotation : NSObject, MKAnnotation {
+    
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(coordinate: CLLocationCoordinate2D, title: String, subtitle: String) {
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+    }
+    
+}
