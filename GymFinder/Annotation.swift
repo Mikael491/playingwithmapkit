e2c3d37052283540024ae92cdec6cb30ca678d06
Mikael <miketekle14@gmail.com>
2016-10-02 19:42:40 -0700

    created file for annotation subclass

---
+++ GymFinder/Annotation.swift
@@ -0,0 +1,9 @@
+//
+//  Annotation.swift
+//  GymFinder
+//
+//  Created by Mikael Teklehaimanot on 10/2/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import Foundation
